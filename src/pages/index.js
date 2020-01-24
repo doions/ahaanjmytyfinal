da3e10a017c6478dcc5dbf29045f363721aab1f9
Doions <43123876+doions@users.noreply.github.com>
2020-01-24 12:07:13 +0530

    Image on top

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,12 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi. I'm Ahaan!</h1>
+          <Image />
+
     <p>Welcome to my new website</p>
     <p>I am a six-year-old boy studying at witty international school. I aspire to become an entrepreneur and a tech expert in the future. I enjoy watching peep and chirp animation, Ryan toy review, pink panther. My favourite activity is playing badminton, football and cricket with my friends.</p>
     <p>I live in Malad (West), Mumbai.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
     <Link to="/page-2/">Go to Download Page for the book here!</Link>
   </Layout>
