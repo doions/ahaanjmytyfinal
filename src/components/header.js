bc637335c051b1896d999741882fea46e8f4a41a
Doions <43123876+doions@users.noreply.github.com>
2020-01-24 11:46:21 +0530

    aqua

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `aqua`,
             textDecoration: `none`,
           }}
         >
