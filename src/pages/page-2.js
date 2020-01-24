d7e6bd99e1e7f149f34bde80a89b63611692d070
Doions <43123876+doions@users.noreply.github.com>
2020-01-24 15:09:12 +0530

    ebook correction

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -12,12 +12,13 @@ const SecondPage = () => (
   Here! I am presenting you a free ebook to all of you about internet safety for kids.
   the book has an interesting conversation with an internet bot and kids.
   Please enjoy the book and share your feedback on my website. the comment section will be active pretty soon.
-  
+</p>  
   <br>
   
   <a href="https://manav.myty.in/myty-ebook.pdf">Download</a>
-  </p>
-    <Link to="/">Go back to the homepage</Link>
+   
+  
+  <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
