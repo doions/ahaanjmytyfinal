f468ab06fd5b513ff26a8eb214cc3fc3d3f7dd0a
Doions <43123876+doions@users.noreply.github.com>
2020-01-24 15:15:55 +0530

    attemot

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,17 +6,17 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Ebook myty download Link" />
     <h1>Hi, Welcome to the ebook download page.</h1>
   <p>
   Here! I am presenting you a free ebook to all of you about internet safety for kids.
   the book has an interesting conversation with an internet bot and kids.
   Please enjoy the book and share your feedback on my website. the comment section will be active pretty soon.
+  
+    <a href="https://manav.myty.in/myty-ebook.pdf">Download</a>
+
 </p>  
-  <br>
-  <div>
-  <a href="https://manav.myty.in/myty-ebook.pdf">Download</a>
-   </div>
+  
   
   <Link to="/">Go back to the homepage</Link>
   </Layout>
