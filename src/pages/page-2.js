0fbd1c6efb2d202002f8cf9baf9a15547067a1a8
Doions <43123876+doions@users.noreply.github.com>
2020-01-24 15:42:23 +0530

    Bold and bigger

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -12,8 +12,9 @@ const SecondPage = () => (
   Here! I am presenting you a free ebook to all of you about internet safety for kids.
   the book has an interesting conversation with an internet bot and kids.
   Please enjoy the book and share your feedback on my website. the comment section will be active pretty soon.
+  </p><p>
   
-    <a href="https://manav.myty.in/myty-ebook.pdf">Download</a>
+    <h1><a href="https://manav.myty.in/myty-ebook.pdf">Download</a></h1>
 
 </p>  
   
